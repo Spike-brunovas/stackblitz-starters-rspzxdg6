f8c846d619aa2056c36bb9be4a394861de9b62e0
Spike-brunovas <brunovasr@edu.unifor.br>
2026-09-21 21:00:35 -0300

    commit_calculadora_stackblitz

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('Hello!');
+const numero1 = document.getElementById("numero1");
+const numero2 = document.getElementById("numero2");
+const operacao = document.getElementById("operacao");
+const calcular = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
+
+calcular.addEventListener("click", function() {
+
+  let n1 = Number(numero1.value);
+  let n2 = Number(numero2.value);
+  let op = operacao.value;
+  let resposta;
+
+  if (numero1.value === "" || numero2.value === "") {
+
+    resultado.textContent = "Digite os dois números.";
+
+    return;
+  }
+
+  if (op === "+") {
+    resposta = n1 + n2;
+  }
+
+  else if (op === "-") {
+    resposta = n1 - n2;
+  }
+
+  else if (op === "*") {
+    resposta = n1 * n2;
+  }
+
+  else if (op === "/") {
+
+    if (n2 === 0) {
+      resultado.textContent = "Não é possível dividir por zero.";
+      return;
+    }
+
+    resposta = n1 / n2;
+  }
+
+  resultado.textContent = "Resultado: " + resposta;
+});
